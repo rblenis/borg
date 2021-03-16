97618cc37fac663a89e5a468b83366d850a1fa77
Robert Blenis <r.blenis@visionxyz.com>
2021-03-16 04:16:37 -0400

    fix pep8 violations - testsuite/compress.py

--- src/borg/testsuite/compress.py
+++ src/borg/testsuite/compress.py
@@ -7,7 +7,7 @@ except ImportError:
 
 import pytest
 
-from ..compress import get_compressor, Compressor, CompressionSpec, CNONE, ZLIB, LZ4, LZMA, ZSTD, Auto
+from ..compress import get_compressor, Compressor, CompressionSpec, CNONE, ZLIB, LZ4, LZMA, ZSTD
 
 
 buffer = bytes(2**16)
