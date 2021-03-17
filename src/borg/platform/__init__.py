fb51c4d3f282abe748f64750f8aa4b743e76e8a4
Robert Blenis <r.blenis@visionxyz.com>
2021-03-17 11:42:33 -0400

    fix some pep8 violations in platform/__init__.py

--- src/borg/platform/__init__.py
+++ src/borg/platform/__init__.py
@@ -6,8 +6,7 @@ Platform-specific APIs.
 Public APIs are documented in platform.base.
 """
 
-from .base import acl_get, acl_set
-from .base import set_flags, get_flags
+from .base import acl_get, acl_set, set_flags, get_flags
 from .base import SaveFile, SyncFile, sync_dir, fdatasync, safe_fadvise
 from .base import swidth, API_VERSION
 from .base import process_alive, get_process_id, local_pid_alive, fqdn, hostname, hostid
@@ -15,19 +14,14 @@ from .base import process_alive, get_process_id, local_pid_alive, fqdn, hostname
 OS_API_VERSION = API_VERSION
 
 if not sys.platform.startswith(('win32', )):
-    from .posix import process_alive, local_pid_alive
+    from .posix import process_alive, local_pid_alive   # noqa: F811
 
 if sys.platform.startswith('linux'):  # pragma: linux only
     from .linux import API_VERSION as OS_API_VERSION
-    from .linux import acl_get, acl_set
-    from .linux import set_flags, get_flags
-    from .linux import SyncFile
-    from .linux import swidth
+    from .linux import acl_get, acl_set, set_flags, get_flags, swidth, SyncFile     # noqa: F811
 elif sys.platform.startswith('freebsd'):  # pragma: freebsd only
     from .freebsd import API_VERSION as OS_API_VERSION
-    from .freebsd import acl_get, acl_set
-    from .freebsd import swidth
+    from .freebsd import acl_get, acl_set, swidth       # noqa: F811
 elif sys.platform == 'darwin':  # pragma: darwin only
     from .darwin import API_VERSION as OS_API_VERSION
-    from .darwin import acl_get, acl_set
-    from .darwin import swidth
+    from .darwin import acl_get, acl_set, swidth        # noqa: F811
